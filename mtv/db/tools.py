772b43c000681bb73e9b66d17e001b232364373b
dyuliu <ustdongyu@gmail.com>
2019-10-30 11:20:45 -0400

    fix lint problem

--- mtv/db/tools.py
+++ mtv/db/tools.py
@@ -1,14 +1,14 @@
 
 import logging
-from pymongo import MongoClient
-from bson import ObjectId
 
+from pymongo import MongoClient
 
 LOGGER = logging.getLogger(__name__)
 
 
 def merge_databases():
     pass
+
 
 def delete_datasets():
     cli = MongoClient('localhost', port=27017)
@@ -35,9 +35,8 @@ def prune_dataruns():
     db = cli['mtv-nasa-lstm']
     col = db['datarun']
 
-    needed = ['T-1', 'E-8', 'E-9', 'P-2', 'P-3', 'P-4', 'P-11', 
+    needed = ['T-1', 'E-8', 'E-9', 'P-2', 'P-3', 'P-4', 'P-11',
               'A-9', 'A-8', 'S-1', 'E-2']
-
 
     for x in col.find():
         doc = db['signal'].find_one({'_id': x['signal']})
@@ -50,20 +49,20 @@ def prune_dataruns():
         doc = col.find_one(x['datarun'])
         if doc is None:
             db['prediction'].delete_one({'_id': x['_id']})
-    
+
     for x in db['raw'].find():
         doc = col.find_one(x['datarun'])
         if doc is None:
             db['raw'].delete_one({'_id': x['_id']})
 
+
 def test_area():
     print('test_area')
     LOGGER.info('test_area')
-    pass
+
 
 def main():
     # test_area()
 
     # prune_dataruns()
     delete_datasets()
-
