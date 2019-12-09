790a3c24972037fbc1a5735f1360e7e1aa8aadb0
sergiu-ojoc <sergiu.ojoc@bytex.ro>
2019-12-09 11:00:36 +0200

    Experiments page unit tests

--- src/components/Landing/Experiments.test.js
+++ src/components/Landing/Experiments.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import Experiments from './Experiments';
 import { experiments } from '../../tests/testmocks/experiments';
 import { renderWithStore } from '../../tests/utils';
-import { selectExperiment } from '../../model/actions/landing';
-import { getSelectedExperiment } from '../../model/selectors/projects';
+import * as actions from '../../model/actions/landing';
 
 describe('Testing experiments component', () => {
   const currentState = {
@@ -13,17 +12,19 @@ describe('Testing experiments component', () => {
     },
   };
 
+  const expComponent = renderWithStore(currentState, <Experiments />);
+  const expItem = expComponent.find('.cell').first();
 
-    const expComponent = renderWithStore(currentState, <Experiments />);
-    const expItem = expComponent.find('.cell').first();
+  it('Should render experiment container', () => expect(expComponent).toHaveLength(1));
+  it('Should render experiment heading', () => expect(expComponent.find('h2').text()).toBe('Experiments'));
+  it('Should render pipeline name ', () => expect(expItem.find('h3').text()).toContain('lstm'));
 
-    it('Should render experiment container', () => expect(expComponent).toHaveLength(1));
-    it('Should render experiment heading', () => expect(expComponent.find('h2').text()).toBe('Experiments'));
-    it('Should render pipeline name ', () => expect(expItem.find('h3').text()).toContain('lstm'));
+  it('Should handle onSelectExperiment', () => {
+    const spy = jest.spyOn(actions, 'selectExperiment');
 
-    it('Should handle onSelectExperiment', () => { // @TODO implement this test
-      expect(expItem.getDOMNode().classList.contains('active')).toBe(false);
-      expItem.simulate('click');
-      expect(expItem.getDOMNode().classList.contains('active')).toBe(true);
-    });
+    expect(expItem.getDOMNode().classList.contains('active')).toBe(false);
+    expItem.simulate('click');
+    expect(spy).toHaveBeenCalled();
+    expect(expItem.getDOMNode().classList.contains('active')).toBe(true);
+  });
 });
