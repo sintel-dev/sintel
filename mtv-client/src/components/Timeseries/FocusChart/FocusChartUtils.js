da9f7363c12a7a9ee93f289b69a462a3522f1bf5
sergiu-ojoc <sergiu.ojoc@bytex.ro>
2020-01-21 14:17:38 +0200

    Adding chart data (lines) to the graph

--- mtv-client/src/components/Timeseries/FocusChart/FocusChartUtils.js
+++ mtv-client/src/components/Timeseries/FocusChart/FocusChartUtils.js
@@ -10,7 +10,11 @@ const offset = {
 const pageCoords = {
   width: 0,
   height: 0,
+  translateTop: 88,
+  translateLeft: 38,
 };
+
+const transition = d3.transition().duration(500);
 
 function getScale(dataRun) {
   const { offsetWidth, offsetHeight, minValue, maxValue } = offset;
@@ -36,25 +40,26 @@ function getScale(dataRun) {
 }
 
 const drawChartAxis = (chartID, datarun) => {
-  const transition = d3.transition().duration(500);
   const graph = document.querySelector('#focusGraph');
-  const { width, height } = pageCoords;
+  const { width, height, translateTop, translateLeft } = pageCoords;
   const { xCoord, yCoord } = getScale(datarun);
   const xAxis = d3.axisBottom(xCoord);
   const yAxis = d3.axisLeft(yCoord);
 
-  const chart = d3.select(chartID)
-    .append('svg')
-    .attr('id', 'focusGraph')
-    .attr('width', width)
-    .attr('height', height)
-    .attr('class', 'focus-chart');
 
   if (!graph) {
+    const chart = d3.select(chartID)
+      .append('svg')
+      .attr('id', 'focusGraph')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('class', 'focus-chart');
+
+    drawChartData(datarun); // Should reside here, it's drawing data first and axes after (z-index)
     const axisG = chart
       .append('g')
       .attr('class', 'chart-axis')
-      .attr('transform', 'translate(38, 0)');
+      .attr('transform', `translate(${translateLeft}, 0)`);
 
     axisG.append('g')
       .attr('transform', `translate(0, ${height - 22})`)
@@ -63,22 +68,57 @@ const drawChartAxis = (chartID, datarun) => {
 
     axisG.append('g')
       .attr('class', 'axis axis--y')
-      .attr('transform', 'translate(0, 88)')
+      .attr('transform', `translate(0, ${translateTop})`)
       .call(yAxis.ticks(5, ',f'));
   } else {
+    drawChartData(datarun); // Should reside here, it's drawing data first and axes after (z-index)
     const focusGraph = d3.select('#focusGraph');
     focusGraph
       .select('.axis.axis--x')
+      .attr('transform', `translate(0, ${height - 22})`)
       .transition(transition)
       .call(xAxis);
 
       focusGraph
         .select('.axis.axis--y')
+        .attr('transform', `translate(0, ${translateTop})`)
         .transition(transition)
         .call(yAxis.ticks(5, ',f'));
     }
 };
 
+
+const drawChartData = (datarun) => {
+  const { xCoord, yCoord } = getScale(datarun);
+  const { width, height, translateTop, translateLeft } = pageCoords;
+  const path = d3.select('.chart-data');
+  const chart = d3.select('#focusGraph');
+
+
+  const line = d3
+    .line()
+    .x(d => xCoord(d[0]))
+    .y(d => yCoord(d[1]));
+
+  // eslint-disable-next-line no-underscore-dangle
+  if (path._groups[0][0] === null) {
+    const chartLine = chart
+      .append('g')
+      .attr('class', 'chart-data')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('transform', `translate(${translateLeft}, ${translateTop})`);
+
+    chartLine.append('path')
+      .attr('class', 'chart-waves')
+      .transition().duration(500)
+      .attr('d', () => line(datarun.timeSeries));
+  } else {
+    d3.select('.chart-waves')
+      .transition().duration(500)
+      .attr('d', () => line(datarun.timeSeries));
+  }
+};
 
 export function drawChart(width, height, datarun) {
   pageCoords.width = width;
