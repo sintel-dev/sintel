5f20def61f9511f2c58f744ff6a38d42e2aaa995
dyuliu <ustdongyu@gmail.com>
2020-03-01 20:21:36 -0500

    uncomment all user test

--- mtv-client/src/model/utils/apiUser.test.ts
+++ mtv-client/src/model/utils/apiUser.test.ts
@@ -3,146 +3,146 @@ import axios, { AxiosError } from 'axios';
 
 // jest.mock('axios');
 
-// describe('User auth unit test', () => {
-//   it('signup succeed', async () => {
-//     const responseSignup = await axios({
-//       baseURL: 'http://127.0.0.1:3000/api/v1/',
-//       url: 'users/signup/',
-//       method: 'post',
-//       data: {
-//         email: 'ucenter.mtv@gmail.com',
-//         name: 'testuser',
-//       },
-//     });
+describe('User auth unit test', () => {
+  it('signup succeed', async () => {
+    const responseSignup = await axios({
+      baseURL: 'http://127.0.0.1:3000/api/v1/',
+      url: 'users/signup/',
+      method: 'post',
+      data: {
+        email: 'ucenter.mtv@gmail.com',
+        name: 'testuser',
+      },
+    });
 
-//     console.log(responseSignup.data);
-//     expect(responseSignup.data.message).toMatch('password has been sent');
-//   }, 10000);
+    console.log(responseSignup.data);
+    expect(responseSignup.data.message).toMatch('password has been sent');
+  }, 10000);
 
-//   it('signup failed due to dumplicated email', async () => {
-//     try {
-//       await axios({
-//         baseURL: 'http://127.0.0.1:3000/api/v1/',
-//         url: 'users/signup/',
-//         method: 'post',
-//         data: {
-//           email: 'ucenter.mtv@gmail.com',
-//           name: 'dyuliu',
-//         },
-//       });
-//     } catch (e) {
-//       const _e = e as AxiosError;
-//       expect(_e.response.status).toBe(400);
-//     }
-//   });
+  it('signup failed due to dumplicated email', async () => {
+    try {
+      await axios({
+        baseURL: 'http://127.0.0.1:3000/api/v1/',
+        url: 'users/signup/',
+        method: 'post',
+        data: {
+          email: 'ucenter.mtv@gmail.com',
+          name: 'dyuliu',
+        },
+      });
+    } catch (e) {
+      const _e = e as AxiosError;
+      expect(_e.response.status).toBe(400);
+    }
+  });
 
-//   it('signin succeed', async () => {
-//     const responseSignin = await axios({
-//       baseURL: 'http://127.0.0.1:3000/api/v1/',
-//       url: 'users/signin/',
-//       method: 'post',
-//       data: {
-//         email: 'ucenter.mtv@gmail.com',
-//         password: '6BNPSFU7',
-//       },
-//     });
+  it('signin succeed', async () => {
+    const responseSignin = await axios({
+      baseURL: 'http://127.0.0.1:3000/api/v1/',
+      url: 'users/signin/',
+      method: 'post',
+      data: {
+        email: 'ucenter.mtv@gmail.com',
+        password: '6BNPSFU7',
+      },
+    });
 
-//     console.log(responseSignin.data);
-//     expect(responseSignin.data).toHaveProperty('data.token');
-//   });
+    console.log(responseSignin.data);
+    expect(responseSignin.data).toHaveProperty('data.token');
+  });
 
-//   it('signin failed', async () => {
-//     let status: number;
-//     try {
-//       const responseSignin = await axios({
-//         baseURL: 'http://127.0.0.1:3000/api/v1/',
-//         url: 'users/signin/',
-//         method: 'post',
-//         data: {
-//           email: 'ucenter.mtv@gmail.com',
-//           password: 'WRONGPASSWORD',
-//         },
-//       });
-//     } catch (e) {
-//       const _e = e as AxiosError;
-//       status = _e.response.status;
-//     }
-//     expect(status).toBe(401);
-//   });
+  it('signin failed', async () => {
+    let status: number;
+    try {
+      const responseSignin = await axios({
+        baseURL: 'http://127.0.0.1:3000/api/v1/',
+        url: 'users/signin/',
+        method: 'post',
+        data: {
+          email: 'ucenter.mtv@gmail.com',
+          password: 'WRONGPASSWORD',
+        },
+      });
+    } catch (e) {
+      const _e = e as AxiosError;
+      status = _e.response.status;
+    }
+    expect(status).toBe(401);
+  });
 
-//   it('signout succeed', async () => {
-//     const responseSignin = await axios({
-//       baseURL: 'http://127.0.0.1:3000/api/v1/',
-//       url: 'users/signin/',
-//       method: 'post',
-//       data: {
-//         email: 'ucenter.mtv@gmail.com',
-//         password: '6BNPSFU7',
-//       },
-//     });
+  it('signout succeed', async () => {
+    const responseSignin = await axios({
+      baseURL: 'http://127.0.0.1:3000/api/v1/',
+      url: 'users/signin/',
+      method: 'post',
+      data: {
+        email: 'ucenter.mtv@gmail.com',
+        password: '6BNPSFU7',
+      },
+    });
 
-//     const { uid, token } = responseSignin.data.data;
+    const { uid, token } = responseSignin.data.data;
 
-//     let status: number;
-//     try {
-//       const responseSignin = await axios({
-//         baseURL: 'http://127.0.0.1:3000/api/v1/',
-//         url: 'users/signout/',
-//         method: 'get',
-//         params: {
-//           uid,
-//         },
-//         headers: {
-//           Authorization: token,
-//         },
-//       });
-//       status = responseSignin.status;
-//     } catch (e) {
-//       const _e = e as AxiosError;
-//       status = _e.response.status;
-//     }
+    let status: number;
+    try {
+      const responseSignin = await axios({
+        baseURL: 'http://127.0.0.1:3000/api/v1/',
+        url: 'users/signout/',
+        method: 'get',
+        params: {
+          uid,
+        },
+        headers: {
+          Authorization: token,
+        },
+      });
+      status = responseSignin.status;
+    } catch (e) {
+      const _e = e as AxiosError;
+      status = _e.response.status;
+    }
 
-//     expect(status).toBe(204);
-//   });
+    expect(status).toBe(204);
+  });
 
-//   it('reset password succeed', async () => {
-//     const responseSignin = await axios({
-//       baseURL: 'http://127.0.0.1:3000/api/v1/',
-//       url: 'users/signin/',
-//       method: 'post',
-//       data: {
-//         email: 'ucenter.mtv@gmail.com',
-//         password: '6BNPSFU7',
-//       },
-//     });
+  it('reset password succeed', async () => {
+    const responseSignin = await axios({
+      baseURL: 'http://127.0.0.1:3000/api/v1/',
+      url: 'users/signin/',
+      method: 'post',
+      data: {
+        email: 'ucenter.mtv@gmail.com',
+        password: '6BNPSFU7',
+      },
+    });
 
-//     const { uid, token } = responseSignin.data.data;
+    const { uid, token } = responseSignin.data.data;
 
-//     let status: number;
-//     try {
-//       const responseSignin = await axios({
-//         baseURL: 'http://127.0.0.1:3000/api/v1/',
-//         url: 'users/reset/',
-//         method: 'post',
-//         data: {
-//           email: 'ucenter.mtv@gmail.com',
-//         },
-//         params: {
-//           uid,
-//         },
-//         headers: {
-//           Authorization: token,
-//         },
-//       });
-//       status = responseSignin.status;
-//     } catch (e) {
-//       const _e = e as AxiosError;
-//       status = _e.response.status;
-//     }
+    let status: number;
+    try {
+      const responseSignin = await axios({
+        baseURL: 'http://127.0.0.1:3000/api/v1/',
+        url: 'users/reset/',
+        method: 'post',
+        data: {
+          email: 'ucenter.mtv@gmail.com',
+        },
+        params: {
+          uid,
+        },
+        headers: {
+          Authorization: token,
+        },
+      });
+      status = responseSignin.status;
+    } catch (e) {
+      const _e = e as AxiosError;
+      status = _e.response.status;
+    }
 
-//     expect(status).toBe(204);
-//   }, 10000);
-// });
+    expect(status).toBe(204);
+  }, 10000);
+});
 
 describe('User auth integration test', () => {
   it('signin and fetch authorized resource', async () => {
