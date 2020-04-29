7aef9c9237e6bd92a4fdcd3e5f408c72c1b5340c
sergiu-ojoc <sergiu.ojoc@bytex.ro>
2020-04-29 08:53:31 +0300

    Reduced mock sizes for unit tests (generated snapshots will be smaller)

--- client/src/tests/testmocks/dataRun.js
+++ client/src/tests/testmocks/dataRun.js
@@ -1,6 +1,7 @@
 import { timeSeries } from './timeSeries';
 import { datarunEvents } from './datarunEvents';
 import { maxTimeSeries } from './dataRunMaxTimeSeries';
+import { timeseriesPredictions } from './timeSeriesPredictions';
 
 export const dataRun = {
   id: '5da80105abc56689357439e6',
@@ -12,12 +13,13 @@ export const dataRun = {
   events: datarunEvents,
   timeSeries,
   eventWindows: [
-    [450, 579, 0.06778753037169406, '5da802e1abc5668935743a04', null],
-    [3216, 3340, 0.1063191879375398, '5da802e1abc5668935743a05', null],
-    [4326, 4472, 0.12700117335524264, '5da802e1abc5668935743a06', null],
-    [5924, 6074, 0.17587530027941106, '5da802e1abc5668935743a07', null],
-    [8139, 8199, 0.34263584276549913, '5da802e1abc5668935743a08', null],
-    [8268, 8422, 0.4277890585414085, '5da802e1abc5668935743a09', null],
+    [0, 100, 0.06778753037169406, '5da802e1abc5668935743a04', null],
+    [10, 150, 0.1063191879375398, '5da802e1abc5668935743a05', null],
+    [15, 400, 0.12700117335524264, '5da802e1abc5668935743a06', null],
+    [20, 120, 0.17587530027941106, '5da802e1abc5668935743a07', null],
+    [50, 500, 0.34263584276549913, '5da802e1abc5668935743a08', null],
+    [150, 200, 0.4277890585414085, '5da802e1abc5668935743a09', null],
   ],
   maxTimeSeries,
+  timeseriesPred: timeseriesPredictions,
 };
