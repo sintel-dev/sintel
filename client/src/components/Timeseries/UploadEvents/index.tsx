bca3a642cd549ce3418776ad0315322b1c4800c1
sergiu-ojoc <sergiu.ojoc@bytex.ro>
2020-06-19 08:46:51 +0300

    Aggregation modal ready

--- client/src/components/Timeseries/UploadEvents/index.tsx
+++ client/src/components/Timeseries/UploadEvents/index.tsx
@@ -112,7 +112,7 @@ class UploadEvents extends Component<Props, State> {
               <p>Your .JSON file should follow this example:</p>
               <SyntaxHighlighter style={dracula}>{codeString}</SyntaxHighlighter>
               <div className="upload-info">
-                <input type="checkbox" id="hideInfo"></input>
+                <input type="checkbox" id="hideInfo" />
                 <label htmlFor="hideInfo">Do not show me again</label>
               </div>
             </div>
---
+++ client/src/model/actions/aggregationLevels.ts
@@ -0,0 +1,25 @@
+import { timeToMiliseconds } from 'src/components/Timeseries/AggregationLevels/AggregationChart/Utils';
+import { TOGGLE_AGGREGATION_MODAL, SET_AGGREGATION_TIME_LEVEL } from '../types';
+
+export function toggleAggregationModal(currentState: boolean) {
+  return function (dispatch) {
+    dispatch({
+      type: TOGGLE_AGGREGATION_MODAL,
+      isAggregationModalOpen: currentState,
+    });
+    dispatch(setAggregationLevelAction('30 hours'));
+  };
+}
+
+export function setAggregationLevelAction(timeStamp) {
+  return function (dispatch) {
+    const time = timeToMiliseconds(timeStamp);
+    dispatch({
+      type: SET_AGGREGATION_TIME_LEVEL,
+      aggregationTimeLevel: {
+        selectedLevel: timeStamp,
+        timeInMiliseconds: time,
+      },
+    });
+  };
+}
